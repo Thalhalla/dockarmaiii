42d8900b03842a2066613196ed140651895a1be7
Josh Cox <josh@webhosting.coop>
2015-05-15 17:21:21 -0500

    typos in start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 cd /home/steam
-sed -i s/REPLACEME_USERNAME/$STEAM_USERNAME/ steamer.txt
-sed -i s/REPLACEME_PASSWORD/$STEAM_PASSWORD/ steamer.txt
+sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
+sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 cd ~/steamcmd
-sudo -i -u steam ./steamcmd.sh +runscript /home/steam/steamer.txt
-sudo -i -u steam bash /home/steam/run.sh
+./steamcmd.sh +runscript /home/steam/steamer.txt
+bash /home/steam/run.sh
 
