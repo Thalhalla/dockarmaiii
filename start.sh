503c58503ace965334a724548c5143f0183ad7d6
Josh Cox <josh@webhosting.coop>
2015-08-03 17:08:27 -0500

    script

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ sed -i "s/steamuser=REPLACE_USER/steamuser='$STEAM_USERNAME'/" arma3server
 sed -i "s/steampass=REPLACE_PASSWORD/steampass='$STEAM_PASSWORD'/" arma3server
 set_steam_guard_code $STEAM_GUARD_CODE
 yes y|./arma3server install
-bash /run.sh
+script -qc '/usr/bin/bash /run.sh'
