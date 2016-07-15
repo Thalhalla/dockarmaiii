b3426ee7d1ff75b8fbae90c31387801581a8307e
Josh Cox <josh@webhosting.coop>
2016-07-15 15:10:16 -0500

    installdirs

--- assets/start.sh
+++ assets/start.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-cd /assets
+cp /assets/steamer.txt /home/steam/
+cd /home/steam
 sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
-sed -i "s/REPLACEME_PASSWORD/$STEAM_GID/" steamer.txt
+sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
 cd ~/steamcmd
-./steamcmd.sh +runscript /assets/steamer.txt
+./steamcmd.sh +runscript /home/steam/steamer.txt
 bash /assets/run.sh
