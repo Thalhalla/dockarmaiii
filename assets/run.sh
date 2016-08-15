a88bdd783dfc280fd6dd7be4a128891bac5bd86d
James S. Moore <james@webtechhq.com>
2016-08-15 16:39:03 -0500

    Run bash shell for debugging

--- assets/run.sh
+++ assets/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /home/steam/SteamLibrary
-./srcds_run -game csgo -console -usercon +game_type 1 +game_mode 1 +mapgroup mg_demolition +map de_lake
+#cd /home/steam/SteamLibrary
+#./srcds_run -game csgo -console -usercon +game_type 1 +game_mode 1 +mapgroup mg_demolition +map de_lake
+/bin/bash
