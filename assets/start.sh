c6f0e0a02240158b58cdc0e53965a9865a961fb9
James S. Moore <james@webtechhq.com>
2016-08-15 22:13:49 -0500

    Fix steamcmd path

--- assets/start.sh
+++ assets/start.sh
@@ -9,5 +9,5 @@ cd /home/steam
 sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
-bash /home/steam/steamcmd.sh +runscript /home/steam/steamer.txt
+bash /home/steam/steamcmd/steamcmd.sh +runscript /home/steam/steamer.txt
 bash /assets/run.sh
