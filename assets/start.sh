e536cf33a25c4287b56d71a598ec695fe5b4f65c
James S. Moore <james@webtechhq.com>
2016-08-15 20:54:15 -0500

    Updates for steamcmd

--- assets/start.sh
+++ assets/start.sh
@@ -9,6 +9,5 @@ cd /home/steam
 sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
-cd ~/steamcmd
-./steamcmd.sh +runscript /home/steam/steamer.txt
+bash /home/steam/steamcmd.sh +runscript /home/steam/steamer.txt
 bash /assets/run.sh
