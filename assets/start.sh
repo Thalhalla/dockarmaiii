e6a97f4dde77bc8b9bc4c856be40db0e848db4ec
James S. Moore <james@webtechhq.com>
2016-08-16 15:55:36 -0500

    skip run for now
    skip run for now

--- assets/start.sh
+++ assets/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ArmA3
-mkdir -p "/home/steam/.local/share/Arma 3" 
+mkdir -p "/home/steam/.local/share/Arma 3"
 mkdir -p "/home/steam/.local/share/Arma 3 - Other Profiles"
 
 cp /assets/steamer.txt /home/steam/
@@ -10,4 +10,8 @@ sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
 bash /home/steam/steamcmd/steamcmd.sh +runscript /home/steam/steamer.txt
-bash /assets/run.sh
+
+# skip run for now
+#bash /assets/run.sh
+
+bash
