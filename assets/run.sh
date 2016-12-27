2c8152a7e61e026630fae291e85b62de2eb353a0
James S. Moore <james@ohmdocker.com>
2016-12-27 17:59:23 +0000

    tues

--- assets/run.sh
+++ assets/run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 /home/steam/arma3/arma3server -ip=4.31.168.84 -port=2302
-#/bin/bash
--- assets/start.sh
+++ assets/start.sh
@@ -6,12 +6,11 @@ mkdir -p "/home/steam/.local/share/Arma 3 - Other Profiles"
 
 cp /assets/steamer.txt /home/steam/
 cd /home/steam
+ln -s '/home/steam/Steam/steamapps/common/Arma 3 Server' arma3
 sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
-bash /home/steam/steamcmd/steamcmd.sh +runscript /home/steam/steamer.txt
+#bash /home/steam/steamcmd/steamcmd.sh +runscript /home/steam/steamer.txt
 
 # skip run for now
-#bash /assets/run.sh
-
-bash
+bash /assets/run.sh
