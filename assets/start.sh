52429d7442b9868a13af8d24c622ddd24c26c4c0
James S. Moore <james@ohmdocker.com>
2017-01-02 02:17:28 +0000

    arma3

--- assets/start.sh
+++ assets/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo chown -R steam. /home/steam
+#mkdir /home/steam/serverfiles/arma3
+
 # ArmA3
 mkdir -p "/home/steam/.local/share/Arma 3"
 mkdir -p "/home/steam/.local/share/Arma 3 - Other Profiles"
