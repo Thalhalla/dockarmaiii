3496f61b04b167c8f9e4ac84b2dc2494b672326b
Josh Cox <josh@webhosting.coop>
2017-01-04 14:37:09 -0600

    zephyr

--- assets/start.sh
+++ assets/start.sh
@@ -7,7 +7,6 @@ sudo chown -R steam. /home/steam
 mkdir -p "/home/steam/.local/share/Arma 3"
 mkdir -p "/home/steam/.local/share/Arma 3 - Other Profiles"
 cd /home/steam
-tar zxf /opt/steamer/steamcmd_linux.tar.gz
 
 # run the server
 bash /assets/run.sh
--- assets/valve-way.sh
+++ assets/valve-way.sh
@@ -3,11 +3,12 @@
 mkdir -p /home/steam/serverfiles/arma3
 cp /assets/steamer.txt /home/steam/
 cd /home/steam
+tar zxf /opt/steamer/steamcmd_linux.tar.gz
 #ln -s '/home/steam/Steam/steamapps/common/Arma 3 Server' arma3
 sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
-bash /home/steam/steamcmd/steamcmd.sh +runscript /home/steam/steamer.txt
+bash /home/steam/steamcmd.sh +runscript /home/steam/steamer.txt
 
 cd /home/steam/serverfiles/arma3
 ./arma3server -netlog -ip=172.17.0.3 -port=2302 -cfg=/home/steam/serverfiles/arma3/cfg/arma3-server.network.cfg -config=/home/steam/arma3/serverfiles/cfg/arma3-server.server.cfg -mod= -servermod= -bepath= -autoinit -loadmissiontomemory
