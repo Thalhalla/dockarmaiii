447a99d42ef62cd2ba1558a9c744c2755b51e623
Josh Cox <josh@webhosting.coop>
2017-01-04 13:14:56 -0600

    pkg

--- assets/install.sh
+++ assets/install.sh
@@ -9,5 +9,13 @@ sed -i "s/ip=\"0\.0\.0\.0\"/ip=\"$IP\"/" /home/steam/arma3server
 sed -i "s/port=\"2302\"/port=\"$PORT\"/" /home/steam/arma3server
 
 /home/steam/arma3server auto-install
+
+# https://steamcommunity.com/discussions/forum/1/618456760281557146/?ctp=2
 sudo chown -R steam. /home/steam
+cd /home/steam/steamcmd
+rm -r ./linux32/
+mv ./package/tmp/* /home/steam/steamcmd/
+
+# Start
+cd /home/steam
 /home/steam/arma3server start
