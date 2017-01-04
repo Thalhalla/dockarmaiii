0d4553c6f464c1fcdf2ef7107099df230ec92d5e
Josh Cox <josh@webhosting.coop>
2017-01-04 12:18:03 -0600

    quiet

--- assets/install.sh
+++ assets/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/steam
 rm -f arma3server
-wget -c https://gameservermanagers.com/dl/arma3server
+wget -q https://gameservermanagers.com/dl/arma3server
 chmod +x arma3server
 sed -i "s/username/$STEAM_USERNAME/" /home/steam/arma3server
 sed -i "s/password/$STEAM_PASSWORD/" /home/steam/arma3server
