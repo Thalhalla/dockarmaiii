673161ad97902b50a1b156f632927573bd9730e3
Josh Cox <josh@webhosting.coop>
2017-01-04 21:35:24 -0600

    opt/steam is now the home for steamcmd.sh

--- assets/valve-way.sh
+++ assets/valve-way.sh
@@ -3,10 +3,12 @@
 mkdir -p /home/steam/serverfiles/arma3
 cp /assets/steamer.txt /home/steam/
 cd /home/steam
-tar zxf /opt/steamer/steamcmd_linux.tar.gz
 #ln -s '/home/steam/Steam/steamapps/common/Arma 3 Server' arma3
 sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
-bash /home/steam/steamcmd.sh +runscript /home/steam/steamer.txt
+
+cd /opt/steamer
+tar zxf /opt/steamer/steamcmd_linux.tar.gz
+./steamcmd.sh +runscript /home/steam/steamer.txt
 
