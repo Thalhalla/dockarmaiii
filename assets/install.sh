7dadb0f3c5fd15a4965b90fd69fa489b0b5ff178
Josh Cox <josh@webhosting.coop>
2017-01-04 22:44:40 -0600

    opt

--- assets/install.sh
+++ assets/install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-cd /home/steam
+cd /opt/steamer
 rm -f arma3server
-wget -q https://gameservermanagers.com/dl/arma3server
-chmod +x arma3server
 sed -i "s/username/$STEAM_USERNAME/" /home/steam/arma3server
 sed -i "s/password/$STEAM_PASSWORD/" /home/steam/arma3server
 sed -i "s/ip=\"0\.0\.0\.0\"/ip=\"$IP\"/" /home/steam/arma3server
 sed -i "s/port=\"2302\"/port=\"$PORT\"/" /home/steam/arma3server
 
-/home/steam/arma3server auto-install
+cd /home/steam/
+/opt/steamer/arma3server auto-install
 
 # https://steamcommunity.com/discussions/forum/1/618456760281557146/?ctp=2
 sudo chown -R steam. /home/steam
