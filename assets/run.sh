1c1625738996138e82e7a35b6f20db1658c13fd7
Josh Cox <josh@webhosting.coop>
2017-01-05 19:19:35 -0600

    MODS

--- assets/run.sh
+++ assets/run.sh
@@ -12,7 +12,7 @@ cd /home/steam
 cd /home/steam/serverfiles
 while [ 1 ]
 do
-    ./arma3server -netlog -ip=0.0.0.0 -port=$PORT -cfg=/home/steam/serverfiles/cfg/arma3-server.network.cfg -config=/home/steam/serverfiles/cfg/arma3-server.server.cfg -mod= -servermod= -bepath= -autoinit -loadmissiontomemory
+    ./arma3server -netlog -ip=0.0.0.0 -port=$PORT -cfg=/home/steam/serverfiles/cfg/arma3-server.network.cfg -config=/home/steam/serverfiles/cfg/arma3-server.server.cfg -mod=$MODS -servermod= -bepath= -autoinit -loadmissiontomemory
     sleep 3
     echo restarting
     sleep 3
