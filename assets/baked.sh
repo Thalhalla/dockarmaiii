b1949b6a811416dc5f5ac94f24478b84a19af48c
Josh Cox <josh@webhosting.coop>
2017-01-07 14:32:23 -0600

    baked.sh

---
+++ assets/baked.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo chown -R steam. /home/steam
+#mkdir /home/steam/serverfiles/arma3
+
+# ArmA3
+mkdir -p "/home/steam/.local/share/Arma 3"
+mkdir -p "/home/steam/.local/share/Arma 3 - Other Profiles"
+#tail -f /var/steam/log/console/arma3-server-console.log
+cd /home/steam/serverfiles
+while [ 1 ]
+do
+    ./arma3server -netlog -ip=0.0.0.0 -port=$PORT -cfg=/home/steam/serverfiles/cfg/arma3-server.network.cfg -config=/home/steam/serverfiles/cfg/arma3-server.server.cfg -mod=$MODS -servermod= -bepath= -autoinit -loadmissiontomemory
+    sleep 3
+    echo restarting
+    sleep 3
+done
