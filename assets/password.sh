9fc37bacbccfbed5d90caa45972091abbf8d8372
Josh Cox <josh@webhosting.coop>
2017-01-11 20:20:50 -0600

    config

--- assets/password.sh
+++ assets/password.sh
@@ -11,3 +11,10 @@ sed -i  "s/^localClient.*/localClient[]=\{\"$CLIENT_IP\", \"$CLIENT_IP2\", \"$CL
 sed -i  "s/^passwordAdmin.*/passwordAdmin = \"$ADMIN_PASSWORD\";/" ./arma3-server.server.cfg
 sed -i  "s/^hostname.*/hostname = \"$HOSTNAME\";/" ./arma3-server.server.cfg
 sed -i  "s/^password\ =.*/password = \"$GAME_PASSWORD\";/" ./arma3-server.server.cfg
+
+cd /opt/steamer
+chmod +x ./arma3server
+sed -i "s/username/$STEAM_USERNAME/" ./arma3server
+sed -i "s/password/$STEAM_PASSWORD/" ./arma3server
+sed -i "s/ip=\"0\.0\.0\.0\"/ip=\"$IP\"/" ./arma3server
+sed -i "s/port=\"2302\"/port=\"$PORT\"/" ./arma3server
