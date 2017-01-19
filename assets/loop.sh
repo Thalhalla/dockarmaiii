8bc30f20b6e1f928be58498617bd292ffeb9b7c7
Josh Cox <josh@webhosting.coop>
2017-01-19 16:23:05 -0600

    loop

---
+++ assets/loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /home/steam
+# valve-install
+# /assets/valve-install.sh
+# /home/steam/arma3/arma3server -ip=4.31.168.84 -port=2302
+
+# LGSM way
+#/bin/bash /assets/valve-install.sh
+/bin/bash /assets/config.sh
+#tail -f /var/steam/log/console/arma3-server-console.log
+cd /home/steam/serverfiles
+while [ 1 ]
+do
+    sleep 3
+    echo looping, enter the container and do it yourself manually
+    echo this is meant for debugging
+    sleep 900
+done
