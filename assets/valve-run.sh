95c262c3d33c0f94a5a5637cdd951bc85077978f
James S. Moore <james@webtechhq.com>
2017-02-20 12:10:03 -0600

    MODS in env unavailable  in valve-run.sh

--- assets/valve-run.sh
+++ assets/valve-run.sh
@@ -12,7 +12,10 @@ cd /home/steam
 cd /home/steam/serverfiles
 while [ 1 ]
 do
-    ./arma3server -netlog -ip=0.0.0.0 -port=2302 \
+    echo "Starting server with MODS string:";
+    echo $MODS;
+
+    /bin/bash ./arma3server -netlog -ip=0.0.0.0 -port=2302 \
       -cfg=/home/steam/serverfiles/cfg/arma3-server.network.cfg \
       -config=/home/steam/serverfiles/cfg/arma3-server.server.cfg \
       -mod=$MODS \
