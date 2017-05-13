c50d8c365f4aa066bd7de67c85e1b821a8c60e3d
James S. Moore <james@webtechhq.com>
2017-05-13 12:53:04 -0500

    Allow port config, encaps MODS

--- assets/headless-run.sh
+++ assets/headless-run.sh
@@ -14,10 +14,10 @@ echo "MODS = $MODS"
 #tail -f /var/steam/log/console/arma3-server-console.log
 cd /home/steam/serverfiles
 
-cmd="./arma3server -client -connect=$SERVER_IP -ip=$IP -port=2302 "
+cmd="./arma3server -client -connect=$SERVER_IP -ip=$IP -port=$SERVER_PORT "
 cmd+="-password=$GAME_PASSWORD "
 cmd+="-bepath=/home/steam/serverfiles/battleye "
-cmd+="-mods=$MODS "
+cmd+="-mods=\"$MODS\" "
 cmd+="-netlog "
 
 while [ 1 ]
