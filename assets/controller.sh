ac94fc01d4c836ff4d55e428ce298765c46ea83a
Josh Cox <josh@webhosting.coop>
2017-06-20 15:14:55 -0500

    functionalize

--- assets/controller.sh
+++ assets/controller.sh
@@ -1,37 +1,48 @@
 #!/bin/bash
-source /etc/myvars
 
-if [ ! -z ${FORCEUPDATE+x} ]
-  then
-    sudo chown -R steam:steam /home/steam
-    mkdir -p /home/steam/serverfiles/arma3
-    cp /assets/steamer.txt /home/steam/
-    cd /home/steam
-    #ln -s '/home/steam/Steam/steamapps/common/Arma 3 Server' arma3
-    sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
-    sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
-    sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
+main() {
+  # perform pre-install configuration
+  source /etc/myvars
 
-    cd /opt/steamer
-    ./steamcmd.sh +runscript /home/steam/steamer.txt
-    cd /home/steam
-    sudo chown -R steam:steam /home/steam
-    date -I > /home/steam/serverfiles/app_update_date
-fi
+  if [ ! -z ${FORCEUPDATE+x} ] || [ ! -f /home/steam/serverfiles/app_update_date ]
+    then
+      do_update
+  fi
 
-# set vars and startup
-cd /home/steam
+  # perform post-install configuration
+  source /assets/config.sh
 
-# perform post-install configuration
-source /assets/config.sh
+  # and startup
+  startup
+}
 
-#tail -f /var/steam/log/console/arma3-server-console.log
+startup() {
+  cd /home/steam/serverfiles
+  while [ 1 ]
+  do
+    eval ./arma3server $CMDSTRING
+    sleep 3
+    echo restarting
+    sleep 3
+  done
 
-cd /home/steam/serverfiles
-while [ 1 ]
-do
-  eval ./arma3server $CMDSTRING
-  sleep 3
-  echo restarting
-  sleep 3
-done
+}
+
+do_update() {
+  sudo chown -R steam:steam /home/steam
+  mkdir -p /home/steam/serverfiles/arma3
+  cp /assets/steamer.txt /home/steam/
+  cd /home/steam
+  #ln -s '/home/steam/Steam/steamapps/common/Arma 3 Server' arma3
+  sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
+  sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
+  sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
+
+  cd /opt/steamer
+  ./steamcmd.sh +runscript /home/steam/steamer.txt
+  cd /home/steam
+  sudo chown -R steam:steam /home/steam
+  date -I > /home/steam/serverfiles/app_update_date
+}
+
+main "$@"
