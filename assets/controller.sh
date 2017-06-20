62a0cc4dcf8158f4f43cf8f1fc55635ae1733109
Josh Cox <josh@webhosting.coop>
2017-06-20 15:28:49 -0500

    config

--- assets/controller.sh
+++ assets/controller.sh
@@ -9,6 +9,11 @@ main() {
       do_update
   fi
 
+  if [ ! -f /home/steam/steamer.txt ]
+    then
+      do_config
+  fi
+
   # perform post-install configuration
   source /assets/config.sh
 
@@ -29,20 +34,22 @@ startup() {
 }
 
 do_update() {
-  sudo chown -R steam:steam /home/steam
   mkdir -p /home/steam/serverfiles/arma3
+  cd /opt/steamer
+  ./steamcmd.sh +runscript /home/steam/steamer.txt
+  cd /home/steam
+  sudo chown -R steam:steam /home/steam
+  date -I > /home/steam/serverfiles/app_update_date
+}
+
+do_config() {
+  sudo chown -R steam:steam /home/steam
   cp /assets/steamer.txt /home/steam/
   cd /home/steam
   #ln -s '/home/steam/Steam/steamapps/common/Arma 3 Server' arma3
   sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
   sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
   sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
-
-  cd /opt/steamer
-  ./steamcmd.sh +runscript /home/steam/steamer.txt
-  cd /home/steam
-  sudo chown -R steam:steam /home/steam
-  date -I > /home/steam/serverfiles/app_update_date
 }
 
 main "$@"
