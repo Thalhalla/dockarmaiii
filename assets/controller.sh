d849c9a8844781e37e0a94fcc36b62da005341d2
James S. Moore <james@webtechhq.com>
2017-06-22 17:30:13 -0500

    reorder chown

--- assets/controller.sh
+++ assets/controller.sh
@@ -43,13 +43,15 @@ do_app_update() {
 }
 
 do_steamer_config() {
-  sudo chown -R steam:steam /home/steam
+
   cp /assets/steamer.txt /home/steam/
   cd /home/steam
   #ln -s '/home/steam/Steam/steamapps/common/Arma 3 Server' arma3
   sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
   sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
   sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
+
+  sudo chown -R steam:steam /home/steam
 }
 
 main "$@"
